3120b40a0f894ca15820875e0c3b1e3f3568f3a6
Timeu <timeu.fo@gmail.com>
2022-12-08 12:16:12 -0300

    add flask / upd reqs

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+from flask import Flask, render_template
 import pandas as pd
 import datetime
 import calendar
-import json
 
 hoje = datetime.date.today()
 qtd_dias = calendar.monthrange(hoje.year, hoje.month)[1]
 df_digao = pd.read_excel('data.xlsx', sheet_name = "digao", skipfooter=qtd_dias-hoje.day)
-df_farofas = pd.read_excel('data.xlsx', sheet_name = "farofas")
+df_farofas = pd.read_excel('data.xlsx', sheet_name = "farofas", skipfooter=qtd_dias-hoje.day)
 
-dict_teste={}
 for indice,dado in df_digao.iterrows():
-    # dict_teste=name[1].to_dict()
-    if dado["DIA"] == 6:
-        dict_teste=dict(dado)
+    if dado["DIA"] == hoje.day:
+        dict_info=dict(dado)
 
-print(dict_teste["DIA"])
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=dict_info)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
